963056b11abf4c316111748d51af8622466dc186
donkeygeek <donkeygeek1@gmail.com>
2023-04-02 13:27:22 +0200

    Test - Select 16Go de RAM et vérifier la MAJ du prix

--- src/components/tests/Configuration.test.js
+++ src/components/tests/Configuration.test.js
@@ -1,3 +1,4 @@
+import userEvent from "@testing-library/user-event";
 import { rest } from "msw";
 import { server } from "../../mocks/server";
 import { render, screen, waitFor } from "../../test-utils";
@@ -34,4 +35,45 @@ test("Gestion des erreurs RAM et SSD", async () => {
 
   // Situation 2
   //screen.debug();
+});
+
+test("Mise à jour du prix et affichage infos Produit en fonction des capacités sélectionnées", async () => {
+  const user = userEvent.setup();
+  render(<Configuration />);
+
+  // Récuperation des élements à tester
+  // 1) Prix du produit
+  const productPrice = screen.getByRole("heading", {
+    level: 4,
+    name: "1 199,00 €",
+  });
+  // 2) capacité RAM dans les infos Produits
+  const ramCapacity = screen.getByText("8 Go de mémoire unifiée");
+  // 3) capacité SSD dans les infos Produits
+  const ssdCapacity = screen.getByText("SSD de 256 Go");
+
+  // Attendre pour récupérer les 2 <options> (Async/await via axios)
+  await waitFor(async () => {
+    const allRamOptions = await screen.findAllByRole("option");
+    expect(allRamOptions).toHaveLength(2);
+  });
+
+  // Récupérer les <select>
+  const selectElement = screen.getByRole("combobox", {
+    name: /default select/i,
+  });
+  //screen.debug();
+  // Sélectionner "16 Go de mémoire unifiée (230 €)"
+  await user.selectOptions(selectElement, "230");
+  expect(selectElement.value).toBe("230");
+  expect(
+    screen.getByRole("option", {
+      name: /16 Go de mémoire unifiée/i,
+      exact: false,
+    }).selected
+  ).toBe(true);
+
+  // screen.debug();
+  // vérifier le prix du produit suite à la sélection
+  expect(productPrice).toHaveTextContent("1 429,00");
 });
